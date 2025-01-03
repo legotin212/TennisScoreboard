10e470425b3838474d60f38a79a07c7b8f28dfc6
legotin212 <99343368+legotin212@users.noreply.github.com>
2025-01-03 17:46:17 +0500

    feat: Created newMatchController and http filter

---
+++ src/main/java/controller/newMatchController.java
@@ -0,0 +1,18 @@
+package controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class newMatchController extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String firstPlayerName = req.getParameter("PlayerOne");
+        String secondPlayerName = req.getParameter("PlayerTwo");
+//        validator.validate(firstPlayerName,secondPlayerName);
+//        matchService.create(firstPlayerName,secondPlayerName);
+        //redirect to(куда)
+    }
+}
---
+++ src/main/java/filter/UtfFIlter.java
@@ -0,0 +1,14 @@
+package filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+@WebFilter
+public class UtfFIlter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        servletResponse.setContentType("application/json");
+        servletResponse.setCharacterEncoding("UTF-8");
+    }
+}

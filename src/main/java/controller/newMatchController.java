6b0d40dde23e579731e6b3b075c0339a7e91770f
legotin212 <99343368+legotin212@users.noreply.github.com>
2025-01-04 05:33:35 +0500

    feat: imported html/css/js files

--- src/main/java/controller/newMatchController.java
+++ src/main/java/controller/newMatchController.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class newMatchController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+
     }
 
     @Override
